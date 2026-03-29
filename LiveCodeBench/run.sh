f4f588b2495796c9f125baf5f099208f8a170fa8
papaya0481 <932525301@qq.com>
2026-03-29 17:20:56 +0800

    merge

--- LiveCodeBench/run.sh
+++ LiveCodeBench/run.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES=4
-export LD_LIBRARY_PATH=/data/wuli_error/miniconda3/envs/llmqw/lib:$LD_LIBRARY_PATH
+# export CUDA_VISIBLE_DEVICES=4
+export LD_LIBRARY_PATH=/data2/ruixin/miniconda3/envs/llmqw/lib:$LD_LIBRARY_PATH
 
-python -m lcb_runner.runner.main --model Qwen/Qwen3.5-9B \
+python -m lcb_runner.runner.main --model Qwen/Qwen3-0.6B \
      --scenario codegeneration \
      --vllm_max_gpu_memory 0.9 \
      --evaluate
