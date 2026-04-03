1f67728806f2730e8b16a337beabb47e852b7102
BigfufuOuO <923525301@qq.com>
2026-04-03 18:07:02 +0800

    new bench bash

--- LiveCodeBench/run.sh
+++ LiveCodeBench/run.sh
@@ -1,7 +1,7 @@
-# export CUDA_VISIBLE_DEVICES=1
-export LD_LIBRARY_PATH=/data2/ruixin/miniconda3/envs/llmqw/lib:$LD_LIBRARY_PATH
+export CUDA_VISIBLE_DEVICES=3
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
-python -m lcb_runner.runner.main --model Qwen/Qwen3-0.6B \
+python -m lcb_runner.runner.main --model Qwen/Qwen3.5-0.8B \
      --scenario codegeneration \
      --vllm_max_gpu_memory 0.9 \
      --evaluate --n 1 --start_date 2023-01-01
